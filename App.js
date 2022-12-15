bc2b45f8d3da851bfd48d0d3f7df3088784ccac6
Denio <dnhanale@gmail.com>
2022-12-15 07:02:20 +0200

    Revisiting Rock Paper Scissors

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-const choices = ["rock", "paper", "scissors"];
-const winners = [];
+const choices = ["rock", "paper", "scissors"]
 
 function game() {
     //play the game, play five rounds, console based
-    for(let i =1; i <= 5; i++) {
-        playRound(i);
-    }
-    logWinner();
-    
+    playRound();
 }    
 
-function playRound(round) {
+function playRound() {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
-    //console.log(computerSelection);
-    const winner = checkWinner(playerSelection, computerSelection);
-    winners.push(winner);
-    logRound(playerSelection,computerSelection,winner,round);
-    
 }
 
 function playerChoice () {
@@ -35,9 +25,9 @@ function playerChoice () {
         }
         input = input.toLowerCase();
         check = validateInput(input);
-        
+        //console.log(input);
     }
-    return input;
+    //console.log(input);
 }
 
 function computerChoice() {
@@ -51,38 +41,6 @@ function validateInput(choice) {
     } else {
         return false;
     }
-}
-
-function checkWinner(choicePlayer, choiceComputer) {
-    if (choicePlayer === choiceComputer) {
-        return "Tie";
-    } else if (
-        (choicePlayer === "rock" && choiceComputer === "scissors") ||
-        (choicePlayer === "paper" && choiceComputer === "rock") ||
-        (choicePlayer === "scissors" && choiceComputer === "paper") 
-    ) {
-        return "Player"
-    } else {
-        return "Computer"
-    }
-}
-
-function logWinner(){
-    let playerWins = winners.filter((item) => item == "Player").length;
-    let computerWins = winners.filter((item) => item == "Computer").length;
-    let ties = winners.filter((item) => item == "Tie").length;
-    console.log("Results");
-    console.log("Player Wins:", playerWins);
-    console.log("Computer Wins:", computerWins);
-    console.log("Ties:", ties)
-}
-
-function logRound(playerChoice, computerChoice, winner, round){
-    console.log("Round:", round);
-    console.log("Player Choose:",playerChoice);
-    console.log("Computer Choose:", computerChoice);
-    console.log(winner, "Won the Round");
-    console.log("------------------------------------");
 }
 
 game();
